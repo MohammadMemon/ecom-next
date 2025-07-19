644b481ed3241a14d0d03509a39119652ac00415
Memon Mohammed Rafiq <mohammad.78600@outlook.com>
2025-07-19 15:41:57 +0530

    Added New Arrivcals Component

--- src/components/Home/ClientNewArrivals.jsx
+++ src/components/Home/ClientNewArrivals.jsx
@@ -39,7 +39,10 @@ export default function ClientNewArrivals({ products }) {
   }
 
   return (
-    <div className="container w-[95vw] px-0 xs:w-[90vw] xs:p-3 sm:w-[75vw] sm:p-6 mx-auto">
+    <div
+      id="Latest"
+      className="container w-[95vw] px-0 xs:w-[90vw] xs:p-3 sm:w-[75vw] sm:p-6 mx-auto"
+    >
       <h1 className="mb-6 text-2xl font-bold text-center">New Arrivals</h1>
       <div className="grid w-full grid-cols-2 gap-2 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {localProducts.map((product, index) => (

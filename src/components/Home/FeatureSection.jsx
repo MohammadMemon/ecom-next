1cb2f1a7b6547212a0ef524a4cc880d6740b6d36
Memon Mohammed Rafiq <mohammad.78600@outlook.com>
2025-07-27 20:36:30 +0530

    feat(SEO): Feature Section  SEO

--- src/components/Home/FeatureSection.jsx
+++ src/components/Home/FeatureSection.jsx
@@ -26,25 +26,31 @@ export default function FeatureSection() {
   ];
 
   return (
-    <section className="flex items-center justify-center w-full py-8 md:py-12 lg:py-16 bg-primary-foreground">
-      <div className="container grid grid-cols-1 gap-6 px-4 sm:grid-cols-2 lg:grid-cols-4 ">
-        {features.map((feature, index) => {
-          const IconComponent = feature.icon;
-          return (
-            <Card
-              key={index}
-              className="flex flex-col items-center justify-center h-full p-6 transition-transform rounded-lg shadow-md hover:scale-105 backdrop-blur-md bg-white/60 text-[#02D866]"
-            >
-              <CardContent className="flex flex-col items-center gap-3 p-0">
-                <IconComponent className={`w-10 h-10 ${feature.iconColor}`} />
-                <h3 className="text-xl font-bold text-gray-800">
-                  {feature.title}
-                </h3>
-                <p className="text-sm text-gray-600">{feature.description}</p>
-              </CardContent>
-            </Card>
-          );
-        })}
+    <section
+      className="flex items-center justify-center w-full py-8 md:py-12 lg:py-16 bg-primary-foreground"
+      itemScope
+      itemType="https://schema.org/Service"
+    >
+      <div className="container grid grid-cols-1 gap-6 px-4 sm:grid-cols-2 lg:grid-cols-4">
+        {features.map((feature, index) => (
+          <Card
+            key={index}
+            className="flex flex-col items-center justify-center h-full p-6 transition-transform rounded-lg shadow-md hover:scale-105 backdrop-blur-md bg-white/60 text-[#02D866]"
+            itemProp="offers"
+            itemScope
+            itemType="https://schema.org/Offer"
+          >
+            <CardContent className="flex flex-col items-center gap-3 p-0">
+              <feature.icon className="w-10 h-10" />
+              <h3 className="text-xl font-bold text-gray-800" itemProp="name">
+                {feature.title}
+              </h3>
+              <p className="text-sm text-gray-600" itemProp="description">
+                {feature.description}
+              </p>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </section>
   );
